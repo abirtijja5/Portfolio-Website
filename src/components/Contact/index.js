592dd9a021b1b896b6c92c85922deb258360700d
@Abirtijja <abirtijja5@gmail.com>
2024-04-06 03:19:26 +0200

    changes

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -130,7 +130,7 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_tox7kqs', 'template_nv7k7mj', form.current, 'SybVGsYS52j2TfLbi')
+    emailjs.sendForm('service_3yp6gsi', 'template_uvwl86k', form.current, 'HSy56PjQUfFmbGEc8')
       .then((result) => {
         setOpen(true);
         form.current.reset();
